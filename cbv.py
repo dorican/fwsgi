3af3c60229a04cceeeb21d86c76668a6bb11e925
linux <linux@linux.org>
2020-09-02 08:19:31 +0300

    lesson2

--- cbv.py
+++ cbv.py
@@ -5,9 +5,8 @@ class View:
     context = None
     template = None
 
-    def __call__(self, *args, **kwargs):
-        # byte_context = bytes(self.context, 'utf-8')
-        # return '200 OK', byte_context
+    def __call__(self, request):
         out_template = render(self.template, object_list=[{'context': f'{self.context}'}])
         byte_out_template = bytes(out_template, 'utf-8')
         return '200 OK', byte_out_template
+
